68fb4815d4741dbee748ef36d7fed409f840e467
Felix Klein <felix@qbaylogic.com>
2025-05-01 17:34:37 +0200

    GHC 9.12.1 Nix support (#2)
    Extends the utilized Nix flake by multiple devShells for different GHC versions and adds supports for GHC 9.12.1. Updates Coq to Rocq (v9.0.0).
    
    ---------
    
    Co-authored-by: Diego Diverio <diego@monads.fr>

--- flake.nix
+++ flake.nix
@@ -5,18 +5,58 @@
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
+
   outputs = { self, nixpkgs, flake-utils } :
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-          myHsPkgs = pkgs.haskell.packages.ghc910.extend
-            (pkgs.haskell.lib.compose.packageSourceOverrides {
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        ghcVersions = [ "ghc910" "ghc9121" ];
+        defaultGhcVersion = "ghc910";
+
+        makeOverlay = compilerVersion:
+          let
+            overrideFile = ./. + "/nix/override-${compilerVersion}.nix";
+
+            overrides =
+              if builtins.pathExists overrideFile then
+                import overrideFile { inherit pkgs; }
+              else
+                f: p: { };
+
+            hsOverlay =
+              pkgs.haskell.packages.${compilerVersion}.override {
+                inherit overrides;
+              };
+
+            pkgSrcOverrides = pkgs.haskell.lib.compose.packageSourceOverrides {
               ghc-typelits-proof-assist = ./.;
-            });
-      in
-      {
-        devShells.default = myHsPkgs.shellFor {
-          packages = p: [ p.ghc-typelits-proof-assist ];
-          nativeBuildInputs = with myHsPkgs; [ cabal-install ] ++ (with pkgs; [coq]);
-        };
+            };
+          in
+            hsOverlay.extend pkgSrcOverrides;
+
+        overlays = nixpkgs.lib.attrsets.genAttrs ghcVersions makeOverlay;
+
+        makeDevShell = compilerVersion:
+          with overlays.${compilerVersion};
+          shellFor {
+            name = compilerVersion;
+            packages = p: [
+              # project packages
+              p.ghc-typelits-proof-assist
+            ];
+            nativeBuildInputs = [
+              # Haskell dependencies
+              cabal-install
+
+              # other dependencies
+              pkgs.coq
+              pkgs.rocqPackages.stdlib
+            ];
+          };
+
+        shells = pkgs.lib.attrsets.genAttrs ghcVersions makeDevShell;
+      in {
+        devShells = shells // { default = shells.${defaultGhcVersion}; };
       });
 }
